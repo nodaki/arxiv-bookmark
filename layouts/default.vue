19fc91ee8a9aa29bd10763b8956d3f55d28d94a9
akihiro noda <aki.ap11@gmail.com>
2019-09-23 20:10:19 +0900

    mod: Wrapped nuxt content.

--- layouts/default.vue
+++ layouts/default.vue
@@ -31,7 +31,11 @@
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
     </v-app-bar>
-    <nuxt />
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
