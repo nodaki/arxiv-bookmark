07a5ae32d48f669eb4c5100dc845b207e259688a
akihiro noda <aki.ap11@gmail.com>
2019-10-03 22:01:17 +0900

    feat: Gh-pages setting.

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/arxiv-bookmark/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Material+Icons' }
     ]
   },
@@ -87,5 +87,8 @@ export default {
   },
   server: {
     host: '0.0.0.0'
+  },
+  router: {
+    base: '/arxiv-bookmark/'
   }
 }
