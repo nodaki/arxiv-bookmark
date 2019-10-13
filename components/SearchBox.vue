c6136a449e09297f8db37f90af34cd996df5d930
akihiro noda <aki.ap11@gmail.com>
2019-10-13 10:49:10 +0900

    feat: Change selection appearance.

--- components/SearchBox.vue
+++ components/SearchBox.vue
@@ -20,14 +20,38 @@
           >
             <template v-slot:selection="data">
               <v-chip
+                v-if="data.index < 2"
                 v-bind="data.attrs"
                 :input-value="data.selected"
                 close
                 @click="data.select"
                 @click:close="remove(data.item)"
               >
+                <v-avatar left>
+                  <v-icon small>
+                    {{ data.item.icon }}
+                  </v-icon>
+                </v-avatar>
                 <span>{{ data.item.name }}</span>
               </v-chip>
+              <!-- Change selection appearance if more than 2 categories are selected -->
+              <span v-if="data.index === 2" class="grey-text caption">
+                (+{{ selectedCats.length - 2 }} others)
+              </span>
+            </template>
+            <template v-slot:item="data">
+              <template v-if="typeof data.item !== 'object'">
+                <v-list-item-content v-text="data.item" />
+              </template>
+              <template v-else>
+                <v-list-item-avatar>
+                  <v-icon>{{ data.item.icon }}</v-icon>
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <v-list-item-title v-text="data.item.name" />
+                  <v-list-item-subtitle v-text="data.item.subject" />
+                </v-list-item-content>
+              </template>
             </template>
           </v-autocomplete>
         </v-col>
@@ -47,7 +71,20 @@ export default {
   name: 'SearchBox',
   data () {
     return {
-      allCategories: [{ name: 'cs.CV' }, { name: 'stat.ML' }],
+      allCategories: [
+        { header: 'Computer Science' },
+        { name: 'cs.AI', subject: 'Artificial Intelligence', icon: 'face' },
+        { name: 'cs.CV', subject: 'Computer Vision and Pattern Recognition', icon: 'remove_red_eye' },
+        { name: 'cs.LG', subject: 'Learning', icon: 'menu_book' },
+        { name: 'cs.RO', subject: 'Robotics', icon: 'android' },
+        { divider: true },
+        { header: 'Electrical Engineering and Systems Science' },
+        { name: 'eess.AS', subject: 'Audio and Speech Processing', icon: 'volume_up' },
+        { name: 'eess.IV', subject: 'Image and Video Processing', icon: 'videocam' },
+        { divider: true },
+        { header: 'Statistics' },
+        { name: 'stat.ML', subject: 'Machine Learning', icon: 'bar_chart' }
+      ],
       selectedCats: []
     }
   },
