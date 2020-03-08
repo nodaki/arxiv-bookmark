e908b74023ec84b8f1228cb8073bba72c36f3ef3
akihiro noda <aki.ap11@gmail.com>
2020-03-08 17:05:54 +0900

    feat: Define user CRUD.

---
+++ backend/app/app/crud/crud_user.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from app.core.security import verify_password, get_password_hash
+from app.models.user import User
+from sqlalchemy.orm import Session
+
+from app.crud.base import CRUDBase
+from app.schemas.user import UserCreate, UserUpdate
+
+
+class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
+    def get_by_email(self, db_session: Session, *, email: str) -> Optional[User]:
+        return db_session.query(User).filter(User.email == email).first()
+
+    def create(self, db_session: Session, *, obj_in: UserCreate) -> User:
+        db_obj = User(
+                email=obj_in.email,
+                hashed_password=get_password_hash(obj_in.password),
+                name=obj_in.name,
+        )
+        db_session.add(db_obj)
+        db_session.commit()
+        db_session.refresh(db_obj)
+        return db_obj
+
+    def authenticate(self, db_session: Session, *, email: str, password: str) -> Optional[User]:
+        user = self.get_by_email(db_session, email=email)
+        if not user:
+            return None
+        if not verify_password(password, user.hashed_password):
+            return None
+        return user
+
+    def is_active(self, user: User) -> bool:
+        return user.is_active
+
+
+user = CRUDUser(User)
