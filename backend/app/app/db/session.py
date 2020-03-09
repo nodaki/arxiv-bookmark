1a5e7936f5085a362a2cf91f4d0ccbbaa4164863
akihiro noda <aki.ap11@gmail.com>
2020-03-09 20:19:25 +0900

    fix: Fix the sqlite error.
    sqlite3.ProgrammingError: SQLite objects created in a thread can only be used in that same thread.

--- backend/app/app/db/session.py
+++ backend/app/app/db/session.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from app.core import config
 
-engine = create_engine(config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI,
+                       pool_pre_ping=True,
+                       connect_args={"check_same_thread": False}
+                       )
 db_session = scoped_session(
         sessionmaker(autocommit=False, autoflush=False, bind=engine)
 )
